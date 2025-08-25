833fa1ca2a6f0c7355f09c34bf0c4eac789cdf4d
Ahmed Salama <81855268+ahmed30238@users.noreply.github.com>
2025-08-25 15:43:59 +0300

    Update API base URL in ApiConstance
    Changed the baseUrl from 'https://hossam.mallahsoft.com/api' to 'https://test.eazifly.com/api' in ApiConstance. Added a comment with the previous URL for reference.

--- lib/core/network/api_constances.dart
+++ lib/core/network/api_constances.dart
@@ -1,5 +1,6 @@
 class ApiConstance {
     ApiConstance._();
   static const String apiUrl = "$baseUrl/user/";
-  static const String baseUrl = "https://hossam.mallahsoft.com/api";
+  static const String baseUrl = "https://test.eazifly.com/api";
 }
+// https://hossam.mallahsoft.com/api
